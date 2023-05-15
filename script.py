8e4b1b044a2ce5d385be9bd4d1c57ce777611128
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-05-14 21:51:16 -0300

    Improving the log and translating to English

--- script.py
+++ script.py
@@ -1,64 +1,132 @@
 import os
-import logging
-from datetime import datetime
-from os import path
 import shutil
 
-
-def ler_log(nome_arquivo):
-    arquivo = open('./logs/' + nome_arquivo, 'r')
-    print(arquivo.read())
+from datetime import datetime
+from os import path
 
 
-def verificar_diretorio():
-    diretorios = ['in', 'out', 'bads', 'logs']
-    for tipo in diretorios:
-        relacao = os.path.exists(tipo)
-        if relacao == False:
-            logging.info('Falta diretório!')
-            exit()
+def generate_log(text):
+    global log
+
+    current_date = datetime.now()
+    date_str = current_date.strftime('%d/%m/%y %H:%M:%S')
+
+    log.append(f'{date_str} {text}\n')
 
 
-def analise_arquivo():
-    arquivos_dir_in = os.listdir('./in/')
-    n = 0
-
-    while n < len(arquivos_dir_in):
-        nome_arquivo = arquivos_dir_in[n]
-        resultado = 0
-        logging.info('')
-        logging.info('Processando o arquivo ' + nome_arquivo)
-        with open('./in/' + nome_arquivo, 'r') as conteudo:
-            texto = conteudo.readlines()
-            num_linha = 0
-            while num_linha < len(texto):
-                linha = texto[num_linha].split()
-                for palavra in linha:
-                    if palavra == 'chave':
-                        resultado = resultado + 1
-                num_linha = num_linha + 1
-        if resultado > 0:
-            logging.info('Chave encontrada!')
-            logging.info('Movendo o arquivo...')
-            shutil.move('./in/' + nome_arquivo, './out/' + nome_arquivo)
-        else:
-            logging.info('Chave não encontrada!')
-            logging.info('Movendo o arquivo...')
-            shutil.move('./in/' + nome_arquivo, './bads/' + nome_arquivo)
-        n = n + 1
+def log_read(file_name):
+    file = open('./logs/' + file_name, 'r')
+    print(file.read())
 
 
-def start():
-    data_atual = datetime.now()
-    data_str = data_atual.strftime('%Y%m%d%H%M%S')
-    nome_arquivo = "teste_" + data_str + ".log"
-    log_file = path.join('./logs/', nome_arquivo)
+def check_directory():
+    generate_log('')
+    generate_log('Checking directories ...')
 
-    logging.basicConfig(level=logging.INFO, filename=log_file, format="%(asctime)s | %(message)s")
-    verificar_diretorio()
-    logging.info('Iniciando o processamento')
-    analise_arquivo()
-    ler_log(nome_arquivo)
+    directories = ['in', 'out', 'bads', 'logs']
+
+    for type in directories:
+        check = os.path.exists(type)
+        
+        if check == False:
+            generate_log('')
+            generate_log(f'Missing directory {type}!')
+            generate_log(f'Creating directory {type} ...')
+            
+            os.mkdir(f'./{type}')
 
 
-start()
+def analyze_file():
+    generate_log(f'')
+    generate_log(f'Starting file analysis ...')
+
+    files_dir_in = os.listdir('./in/')
+    
+    if len(files_dir_in) == 0:
+        generate_log(f'There are no files to process ...')
+    else:
+        generate_log(f'There are {len(files_dir_in)} file(s) to process ...')
+        counter = 0
+
+        while counter < len(files_dir_in):
+            file_name = files_dir_in[counter]
+            result = 0
+
+            generate_log('')
+            generate_log('Processing the file ' + file_name)
+
+            with open('./in/' + file_name, 'r') as content:
+                text = content.readlines()
+                num_line = 0
+                
+                while num_line < len(text):
+                    line = text[num_line].split()
+                    
+                    for word in line:                
+                        
+                        if word == 'key':
+                            result = result + 1
+                    
+                    num_line = num_line + 1
+            
+            if result > 0:
+                generate_log('Key found!')
+                generate_log('Moving file to out directory ...')
+
+                shutil.move('./in/' + file_name, './out/' + file_name)
+            else:
+                generate_log('Key not found!')
+                generate_log('Moving file to bads directory...')
+
+                shutil.move('./in/' + file_name, './bads/' + file_name)
+            
+            counter = counter + 1
+
+
+if __name__ == '__main__':
+    error_num = 0
+    log = []
+
+    generate_log('=============================================')
+    generate_log('===============File Manager==================')
+    generate_log('=============================================')
+    generate_log('')
+
+    current_date = datetime.now()
+    date_str = current_date.strftime('%Y%m%d%H%M%S')
+    file_name = "log_" + date_str + ".log"
+    log_file = path.join('./logs/', file_name)
+    
+    generate_log('Starting processing ...')
+
+    try:
+        check_directory()
+    except Exception as e:
+        log.append('Failed to check directory ...')
+        log.append(f'Error: {e}')
+        error_num += 1
+
+    try:
+        analyze_file()
+    except Exception as e:
+        generate_log('')
+        generate_log('Failed to analyze file')
+        generate_log(f'Error: {e}')
+        error_num += 1
+
+    if error_num == 0:
+        generate_log('')
+        generate_log('=============================================')
+        generate_log('Final Status: SUCCESS')
+        generate_log('=============================================')
+    else:
+        generate_log('')
+        generate_log('=============================================')
+        generate_log(f'Final Status: ERROR: Were found {error_num} error(s) ...')
+        generate_log('=============================================')
+
+
+    with open(log_file, 'w+') as log_recorder:
+        log_recorder.writelines(log)
+
+    log_read(file_name)
